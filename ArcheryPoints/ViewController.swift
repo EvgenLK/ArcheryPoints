a18d118e67ef7b62aa24996e7f1f6f7916da2830
Evgenii <evgenleo0512@gmail.com>
2023-01-05 22:08:43 +1000

    new app

--- ArcheryPoints/ViewController.swift
+++ ArcheryPoints/ViewController.swift
@@ -6,12 +6,49 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+        //Ссылка на appDelegate
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        //Создание контекста
+        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        //Описание сущности
+        let entityDescription = NSEntityDescription.entity(forEntityName: "ResultPoint", in: context)
+        
+        //Создание обьекта
+        let managedObject = NSManagedObject(entity: entityDescription!, insertInto: context)
+        
+        //Установка значений атрибутов
+        managedObject.setValue(2, forKey: "numberSet")
+        managedObject.setValue(20, forKey: "countSet")
+        
+        //Извлекаем значение атрибутов
+        let numberSet = managedObject.value(forKey: "numberSet")
+        let countSet = managedObject.value(forKey: "countSet")
+        print("\(numberSet),\(countSet)")
+        //24:30
+        
+        // сохранение данных
+        appDelegate.saveContext()
+        
+        // Извлекаем данные
+        let featchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ResultPoint")
+        do{
+            let results = try context.fetch(featchRequest)
+            for result in results as! [NSManagedObject]{
+                print("number set - \(result.value(forKey: "numberSet")!), count set- \(result.value(forKey: "countSet")!)")
+            }
+        }catch{
+            print(error)
+        }
     }
 
 
